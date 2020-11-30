53a3a87adf87877be48f958d1948e030665722a4
Takuchan1102 <takuchans631102@gmail.com>
2020-11-30 22:26:18 +0900

    Second Commit

--- app/src/main/java/jp/techacademy/takuya/calcapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/takuya/calcapp/MainActivity.kt
@@ -6,6 +6,12 @@ import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import android.content.Intent
 import java.text.Format
+import android.support.v4.app.SupportActivity
+import android.support.v4.app.SupportActivity.ExtraData
+import android.support.v4.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
         override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,17 +24,19 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
         override fun onClick(v: View) {
                 val intent = Intent(this, SecondActivity::class.java)
+                val a = editText1.toString().toDouble()
+                val b = editText2.toString().toDouble()
                 if (v.id == R.id.button1) {
-                        intent.putExtra("VALUE",editText1 + editText2)
+                        intent.putExtra("VALUE",a+b)
                         startActivity(intent)
                 } else if (v.id == R.id.button2) {
-                        intent.putExtra("VALUE",editText1 - editText2)
+                        intent.putExtra("VALUE",a-b)
                         startActivity(intent)
                 } else if (v.id == R.id.button3) {
-                        intent.putExtra("VALUE", editText1 * editText2)
+                        intent.putExtra("VALUE", a*b)
                         startActivity(intent)
                 } else if (v.id == R.id.button4) {
-                        intent.putExtra("VALUE",editText1 / editText2)
+                        intent.putExtra("VALUE",a/b)
                         startActivity(intent)
         }
         }
