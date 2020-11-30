f4142b61d3a37ef9c4987c0be25c1ccf89d362b9
Takuchan1102 <takuchans631102@gmail.com>
2020-11-30 22:59:32 +0900

    Third Commit

--- app/src/main/java/jp/techacademy/takuya/calcapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/takuya/calcapp/MainActivity.kt
@@ -17,10 +17,10 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         override fun onCreate(savedInstanceState: Bundle?) {
                 super.onCreate(savedInstanceState)
                 setContentView(R.layout.activity_main)
-                button1.setOnClickListener {this}
-                button2.setOnClickListener {this}
-                button3.setOnClickListener {this}
-                button4.setOnClickListener {this}
+                button1.setOnClickListener (this)
+                button2.setOnClickListener (this)
+                button3.setOnClickListener (this)
+                button4.setOnClickListener (this)
         }
         override fun onClick(v: View) {
                 val intent = Intent(this, SecondActivity::class.java)
