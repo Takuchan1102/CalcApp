a3eb0d0af15c071b217f540e59ad878107f95c70
Takuchan1102 <takuchans631102@gmail.com>
2020-12-05 08:25:30 +0900

    Fifth Commit

--- app/src/main/java/jp/techacademy/takuya/calcapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/takuya/calcapp/MainActivity.kt
@@ -1,45 +1,47 @@
 package jp.techacademy.takuya.calcapp
-
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import android.content.Intent
-import java.text.Format
-import android.support.v4.app.SupportActivity
-import android.support.v4.app.SupportActivity.ExtraData
-import android.support.v4.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-import android.text.TextUtils
-import android.text.TextUtils.*
-
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-        override fun onCreate(savedInstanceState: Bundle?) {
-                super.onCreate(savedInstanceState)
-                setContentView(R.layout.activity_main)
-                button1.setOnClickListener (this)
-                button2.setOnClickListener (this)
-                button3.setOnClickListener (this)
-                button4.setOnClickListener (this)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button1.setOnClickListener(this)
+        button2.setOnClickListener(this)
+        button3.setOnClickListener(this)
+        button4.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        if (!editText1.toString().isEmpty() && !editText2.toString().isEmpty()) {
+            val intent = Intent(this, SecondActivity::class.java)
+            val a = editText1.text.toString()
+            val b = editText2.text.toString()
+            if (v.id == R.id.button1) {
+                intent.putExtra("VALUE1", a)
+                intent.putExtra("VALUE2", b)
+                startActivity(intent)
+            }
+            if (v.id == R.id.button2) {
+                intent.putExtra("VALUE3", a)
+                intent.putExtra("VALUE4", b)
+                startActivity(intent)
+            }
+            if (v.id == R.id.button3) {
+                intent.putExtra("VALUE5", a)
+                intent.putExtra("VALUE6", b)
+                startActivity(intent)
+            }
+            if (v.id == R.id.button4) {
+                intent.putExtra("VALUE7", a)
+                intent.putExtra("VALUE8", b)
+                startActivity(intent)
+            }
         }
-        override fun onClick(v: View) {
-            if(!editText1.toString().isEmpty() && !editText2.toString().isEmpty()){
-                val intent = Intent(this, SecondActivity::class.java)
-                val a = editText1.toString().toDouble()
-                var b = editText2.toString().toDouble()
-                  if (v.id == R.id.button1) {
-                        intent.putExtra("VALUE",a+b)
-                        startActivity(intent)
-                } else if (v.id == R.id.button2) {
-                        intent.putExtra("VALUE",a-b)
-                        startActivity(intent)
-                } else if (v.id == R.id.button3) {
-                        intent.putExtra("VALUE", a*b)
-                        startActivity(intent)
-                } else if (v.id == R.id.button4) {
-                        intent.putExtra("VALUE",a/b)
-                        startActivity(intent)}
-        }
-        }}
+    }
+
+}
 
--- app/src/main/java/jp/techacademy/takuya/calcapp/SecondCalcAppMain2Activity.kt
+++ app/src/main/java/jp/techacademy/takuya/calcapp/SecondCalcAppMain2Activity.kt
@@ -1,14 +1,22 @@
 package jp.techacademy.takuya.calcapp
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_second_calc_app_main2.*
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second_calc_app_main2)
-        val value1 = intent.getIntExtra("VALUE1",0)
-        textView.text = "${value1}"
-    }
-}
+        val value1 = intent.getIntExtra("VALUE1", 0)
+        val value2 = intent.getIntExtra("VALUE2", 0)
+        textView.text = "${value1 + value2}"
+        val value3 = intent.getIntExtra("VALUE3", 0)
+        val value4 = intent.getIntExtra("VALUE4", 0)
+        textView.text = "${value3 - value4}"
+        val value5 = intent.getIntExtra("VALUE5", 0)
+        val value6 = intent.getIntExtra("VALUE6", 0)
+        textView.text = "${value5 * value6}"
+        val value7 = intent.getIntExtra("VALUE7", 0)
+        val value8 = intent.getIntExtra("VALUE8", 0)
+        textView.text = "${value7 / value8}"
+    }}
 
